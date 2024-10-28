9a7581e5127fedfd09d8c2aaeb27f4225dfc5881
ZUB-OK <zubokmine@rambler.ru>
2024-10-28 18:57:48 +0700

    Недоделанное второе задание с ошибкой

--- task2/script.sh
+++ task2/script.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-read -p "Введите название проекта" 
-project_name
-read -p "Введите директорию для создания проекта"
-project_directory
+echo "Введите название проекта"
+read name
+echo "Введите директорию для создания проекта"
+read dir
+full = "$dir"/"$name"
 
+echo "$full"
+
+mkdir "$full"
+mkdir "$full"/scr
+mkdir "$full"/scr/scripts
+mkdir "$full"/scr/styles
+mkdir "$full"/scr/images
+
+echo "# $name" > "$full/README.md"
+echo "*.log node_modules/" > "$full/.gitignore"
